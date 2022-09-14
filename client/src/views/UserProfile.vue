9ad176d85f05bd0093e28a230954b603fa53ce91
Sagun Khatri <sagunkhatri@protonmail.com>
2022-09-15 00:40:49 +0545

    [add]: additional information form structure

--- client/src/views/UserProfile.vue
+++ client/src/views/UserProfile.vue
@@ -44,7 +44,7 @@
         <div :style="{ padding: '24px', minHeight: '380px' }">
             <a-row justify="center">
                 <a-col>
-                    <a-card style="width: 400px">
+                    <a-card style="width: 500px">
                         <a-card-meta title="Name" description="role" >
                         <template #avatar>
                             <a-avatar :size="{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }">
@@ -58,13 +58,96 @@
                             <a-typography >User Details</a-typography>
                         </a-divider>
                         <a-descriptions bordered :column="1">
-                            <a-descriptions-item label="Email">Zhou Maomao</a-descriptions-item>
-                            <a-descriptions-item label="Role">Mentor</a-descriptions-item>
+                            <a-descriptions-item label="Email">{{formState.user.email}}</a-descriptions-item>
+                            <a-descriptions-item label="Role">Mentee</a-descriptions-item>
                         </a-descriptions>
                     </a-card>
                 </a-col>
             </a-row>
             <a-divider>Additional Details</a-divider>
+            <a-row justify="center">
+              
+              <a-col :span="8">
+                <a-typography-paragraph>
+                Please fill in the additional details to get the best experience and recommendations.
+              </a-typography-paragraph>
+              <a-divider />
+                <a-form
+                  :model="formState"
+                  layout="vertical"
+                  name="nest-messages"
+                  :validate-messages="validateMessages"
+                  @finish="onFinish"
+                >
+                  <a-form-item 
+                  :name="['user', 'fname']" 
+                  label="Full Name"
+                  :rules="[{ required: true }]">
+                    <a-input placeholder="eg: John Doe" size="large" v-model:value="formState.user.fname" />
+                  </a-form-item>
+
+                  <a-form-item 
+                  :name="['user', 'introduction']" 
+                  label="Introduction"
+                  :rules="[{required: true}]">
+                    <a-textarea placeholder="A few words about yourself." v-model:value="formState.user.introduction" />
+                  </a-form-item>
+
+                  <a-form-item 
+                  :name="['user', 'dob']"
+                  :rules="[{required: true}]"
+                  label="Date of Birth" >
+                    <a-date-picker v-model:value="formState.user.dob" value-format="YYYY-MM-DD" />
+                  </a-form-item>
+
+                  <a-form-item
+                  :name="['user', 'tags']"
+                  :rules="[{required: true}]"
+                  label="Skills and Interests">
+                    <a-select
+                      v-model:value="formState.user.tags"
+                      mode="tags"
+                      style="width: 100%"
+                      placeholder="Select Skills and Interests"
+                      :options="TagsOption"
+                    ></a-select>
+                  </a-form-item>
+
+                  <a-form-item
+                  :name="['user', 'country']"
+                  :rules="[{required: true}]"
+                  label="Country">
+                    <a-select
+                      v-model:value="formState.user.country"
+                      mode="multiple"
+                      style="width: 100%"
+                      placeholder="Select a Country"
+                      :options="countryOptions"
+                    ></a-select>
+                  </a-form-item>
+
+                  <a-form-item
+                  :name="['user', 'languages']"
+                  :rules="[{required: true}]"
+                  label="Languages">
+                    <a-select
+                      v-model:value="formState.user.country"
+                      mode="multiple"
+                      style="width: 100%"
+                      placeholder="Tags Mode"
+                      :options="languageOptions"
+                    ></a-select>
+                  </a-form-item>
+                 
+                  
+                 
+                  
+                  <a-form-item>
+                    <a-button type="primary" html-type="submit">Submit</a-button>
+                  </a-form-item>
+                </a-form>
+              </a-col>
+            </a-row>
 
         </div>
       </a-layout-content>
@@ -78,27 +161,64 @@
     
   </template>
   <script lang="ts">
-  import { defineComponent, ref } from 'vue';
+  import { defineComponent, ref, reactive } from 'vue';
   import { UserOutlined, 
     SettingOutlined,
     LogoutOutlined,
  } from '@ant-design/icons-vue';
+
+
   export default defineComponent({
     components:{
         UserOutlined,
         SettingOutlined,
         LogoutOutlined,
-
-
     },
+
     setup() {
-      return {
-        selectedKeys: ref<string[]>(['2']),
-      };
-    },
+
+    const validateMessages = {
+      required: '${label} is required!',
+      types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+      },
+      number: {
+        range: '${label} must be between ${min} and ${max}',
+      },
+    };
+
+    const formState = reactive({
+      user: {
+        fname: '',
+        email:'',
+        role:'',
+        tags:[{}],
+        country:'',
+        languages:'',
+        dob: undefined,
+        introduction: '',
+      },
+    });
+    const onFinish = (values: any) => {
+      console.log('Success:', values);
+    };
+    return {
+      formState,
+      onFinish,
+      validateMessages,
+    };
+  },
   });
   </script>
   <style>
+  #app{
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  }
+
   #components-layout-demo-fixed .logo {
     width: 120px;
     height: 31px;
