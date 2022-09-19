d993c967196f5c0738fcb59e8bea6f36ebf596b8
Sagun Khatri <sagunkhatri@protonmail.com>
2022-09-20 02:42:57 +0545

    feat: skeleton for Applications, rooms and mentors page

--- client/src/views/ApplicationView.vue
+++ client/src/views/ApplicationView.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <h1>This is application page.</h1>
-  </div>
+  <a-row justify="space-between">
+    <a-typography-title :level="2">Your Applications</a-typography-title>
+    <a-col>
+      <a-input-search
+        v-model:value="value"
+        placeholder="Search Application"
+        enter-button
+        size="large"
+        @search="onSearch"
+      />
+    </a-col>
+  </a-row>
+
+  
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const value = ref<string>("");
+
+    const onSearch = (searchValue: string) => {
+      console.log("use value", searchValue);
+      console.log("or use this.value", value.value);
+    };
+
+    return {
+      value,
+      onSearch,
+    };
+  },
+});
+</script>
--- client/src/views/RoomView.vue
+++ client/src/views/RoomView.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <h1>This is room page.</h1>
-  </div>
+  <a-row justify="space-between">
+    <a-typography-title :level="2">Your Rooms</a-typography-title>
+    <a-col>
+      <a-input-search
+        v-model:value="value"
+        placeholder="Search Rooms"
+        enter-button
+        size="large"
+        @search="onSearch"
+      />
+    </a-col>
+  </a-row>
+
+  
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const value = ref<string>("");
+
+    const onSearch = (searchValue: string) => {
+      console.log("use value", searchValue);
+      console.log("or use this.value", value.value);
+    };
+
+    return {
+      value,
+      onSearch,
+    };
+  },
+});
+</script>
